021f2399122fce605e619fd3517d8becc76f7597
logeybogey <michaeldcjohnson@gmail.com>
2020-11-02 21:40:56 +0600

    done for the day

--- index.js
+++ index.js
@@ -7,30 +7,37 @@ class toDoCategory {
     }
 }
 
+let categoryCollection = [];
+
 const createNewCategory = () => {
     let newCatName = prompt('Name of your category');
     let newCat = new toDoCategory(newCatName, [4,6,5])
+    categoryCollection.push(newCat)
     console.log(newCat.id)
     console.log(newCat.tasks)
+    console.log(categoryCollection)
     let bottomLeftContainer = document.getElementById('bottomLeftContainer')
 
     let newCategoryContainer = document.createElement('p')
     newCategoryContainer.classList.add('newCategory')
     newCategoryContainer.textContent = newCat.id;
     bottomLeftContainer.appendChild(newCategoryContainer)
-
-    let categories = Array.from(document.getElementsByClassName('newCategory'))
-
-    categories.forEach(category => {
-        category.addEventListener('click', renderTasks(newCat))
-    })
-    return{newCat}
+    setListeners()
 }
 
-const renderTasks = (newCat) => {
+const setListeners = () => {
+let categories = Array.from(document.getElementsByClassName('newCategory'))
+categories.forEach(category => {
+    category.addEventListener('click', renderTasks)
+})
+}
+
+const renderTasks = () => {
+    console.log('poopoo')
     let bottomRightContainer = document.getElementById('bottomRightContainer')
     let tasksDisplay = document.createElement('p');
-    tasksDisplay.textContent = newCat.tasks
+    console.log(categoryCollection[0].tasks)
+    tasksDisplay.textContent = categoryCollection[0].tasks
 
     bottomRightContainer.appendChild(tasksDisplay)
 }
