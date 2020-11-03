7d9937479c0cd6daad7a236c084154723e34aaa8
logeybogey <michaeldcjohnson@gmail.com>
2020-11-03 13:19:14 +0600

    got the array displaying correctlywhen each category is selected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addCategoryButton = document.getElementById('addCategoryButton')
 
+
 class toDoCategory {
     constructor(id, tasks){
         this.id = id;
@@ -8,20 +9,19 @@ class toDoCategory {
 }
 
 let categoryCollection = [];
+let counter = 0;
 
 const createNewCategory = () => {
     let newCatName = prompt('Name of your category');
     let newCat = new toDoCategory(newCatName, [4,6,5])
     categoryCollection.push(newCat)
-    console.log(newCat.id)
-    console.log(newCat.tasks)
-    console.log(categoryCollection)
     let bottomLeftContainer = document.getElementById('bottomLeftContainer')
-
     let newCategoryContainer = document.createElement('p')
     newCategoryContainer.classList.add('newCategory')
     newCategoryContainer.textContent = newCat.id;
     bottomLeftContainer.appendChild(newCategoryContainer)
+    newCategoryContainer.setAttribute('id', `${counter}`)
+    counter ++;
     setListeners()
 }
 
@@ -32,18 +32,25 @@ categories.forEach(category => {
 })
 }
 
-const renderTasks = () => {
-    console.log('poopoo')
+const renderTasks = (e) => {
+    removeTaskContent();
     let bottomRightContainer = document.getElementById('bottomRightContainer')
-    let tasksDisplay = document.createElement('p');
-    console.log(categoryCollection[0].tasks)
-    tasksDisplay.textContent = categoryCollection[0].tasks
-
-    bottomRightContainer.appendChild(tasksDisplay)
+    let selectedCat = e.target.id;
+    let selectedCatTasks = categoryCollection[`${selectedCat}`].tasks
+   
+    selectedCatTasks.forEach(task => {
+        let tasksDisplay = document.createElement('p');
+        tasksDisplay.textContent = task
+        bottomRightContainer.appendChild(tasksDisplay)
+    })
 }
 
-
+const removeTaskContent = () => {
+    let bottomRightContainer = document.getElementById('bottomRightContainer')
+    bottomRightContainer.textContent = '';
+}
 
 addCategoryButton.addEventListener('click', createNewCategory)
+
 
 
