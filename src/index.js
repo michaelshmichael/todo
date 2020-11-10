9faecef130315cbb8500dd9a23974e7477f5fa63
logeybogey <michaeldcjohnson@gmail.com>
2020-11-10 21:07:37 +0600

    edited styling colours

--- src/index.js
+++ src/index.js
@@ -106,12 +106,13 @@ const Category = (() => {
     }
     
     function deleteCategory(e) {
-        if (confirm("Delete Category?") == true) {   
+        if (confirm("Delete Category?")) {   
             document.querySelectorAll('.tasksDisplay .completedTasksDisplay').forEach(task => task.remove());
             const index = e.target.dataset.index;
             categoryCollection.splice(index, 1)
             localStorage.setItem('categoryCollection', JSON.stringify(categoryCollection));
             renderCategories()
+            Tasks.renderTasks()
         } 
     }
 
