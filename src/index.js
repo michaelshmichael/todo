8bd6d4228bea8bf8900500405a073e6f7d29f77a
logeybogey <michaeldcjohnson@gmail.com>
2020-11-12 21:31:40 +0600

    gutted out renderTasks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {createNewCategory} from './createNewCategory.js'
 import {displayCategoryHeading} from './displayCategoryHeading.js'
 import {deleteCategory} from './deleteCategory.js'
 import {setActiveCategory} from './setActiveCategory.js'
+import {renderTasks} from './renderTasks.js'
 
 // let categoryCollection
 // if (localStorage.getItem('categoryCollection')) {
@@ -140,91 +141,6 @@ const Tasks = (() => {
         renderTasks()
     }
 
-    function renderTasks() {
-        let counter = 0
-        document.querySelectorAll('.tasksDisplay').forEach(e => e.remove());
-        document.querySelectorAll('.completedTasksDisplay').forEach(e => e.remove());
-        let activeCategory = Category.categoryCollection.find(element => element.active === true);
-        let activeCategoryTasks = activeCategory.tasks
-        activeCategoryTasks.forEach(task => {
-            let tasksDisplay = document.createElement('div');
-            if(task.checklist == false){
-                tasksDisplay.setAttribute('class', 'tasksDisplay')
-            } else {
-                 tasksDisplay.setAttribute('class', 'completedTasksDisplay') 
-            }
-            let taskInfoContainer = document.createElement('div')
-            taskInfoContainer.setAttribute('class', 'taskInfoContainer')
-            bottomRightContainer.appendChild(tasksDisplay)
-            
-            let priorityIndicator = document.createElement('div')
-
-                if(task.priority == 1){
-                    priorityIndicator.classList.add('highPriorityIndicator')
-                } else if (task.priority == 2){
-                    priorityIndicator.classList.add('mediumPriorityIndicator')
-                } else if (task.priority == 3){
-                    priorityIndicator.classList.add('lowPriorityIndicator')
-                }
-
-            let taskNameAndDueDateContainer = document.createElement('div')
-            taskNameAndDueDateContainer.setAttribute('class', 'taskNameAndDueDateContainer')
-
-            let taskName = document.createElement('div')
-            taskName.setAttribute('class', 'taskName')
-            taskName.textContent = `${task.id}`
-
-            let dueDate = document.createElement('div')
-            dueDate.setAttribute('class', 'dueDate')
-            dueDate.textContent = `Due: ${task.dueDate}`
-
-            let notesContainer = document.createElement('div')
-            notesContainer.setAttribute('class', 'notesContainer')
-
-            let notesHeading = document.createElement('div')
-            notesHeading.setAttribute('class', 'notesHeading')
-
-            let notesContent = document.createElement('div')
-            notesContent.setAttribute('class', 'notesContent')
-            notesContent.textContent = `${task.notes}`
-
-            tasksDisplay.appendChild(priorityIndicator)
-            tasksDisplay.appendChild(taskInfoContainer)
-            taskInfoContainer.appendChild(taskNameAndDueDateContainer)
-            taskNameAndDueDateContainer.appendChild(taskName)
-            taskNameAndDueDateContainer.appendChild(dueDate)
-            taskInfoContainer.appendChild(notesContainer)
-            notesContainer.appendChild(notesHeading)
-            notesContainer.appendChild(notesContent)
-            notesHeading.textContent = 'Notes'
-
-            let deleteEditAndCheckContainer = document.createElement('div')
-            deleteEditAndCheckContainer.setAttribute('class', 'deleteEditAndCheckContainer')
-            taskInfoContainer.appendChild(deleteEditAndCheckContainer)
-
-            let deleteTaskIcon = document.createElement('i')
-            deleteTaskIcon.setAttribute('class', 'fa fa-trash deleteTaskIcon')
-            deleteTaskIcon.setAttribute('data-index', `${counter}`)
-            deleteEditAndCheckContainer.appendChild(deleteTaskIcon)
-            
-
-            let editTaskIcon = document.createElement('i')
-            editTaskIcon.setAttribute('class', 'fa fa-edit editTaskIcon')
-            editTaskIcon.setAttribute('data-index', `${counter}`)
-            deleteEditAndCheckContainer.appendChild(editTaskIcon)
-            
-
-            let checkboxComplete = document.createElement('i')
-            checkboxComplete.setAttribute('class', 'fa fa-check-circle checkboxComplete')
-            checkboxComplete.setAttribute('data-index', `${counter}`)
-            
-            deleteEditAndCheckContainer.appendChild(checkboxComplete)
-            counter++
-        })
-        counter = 0
-        addTaskListeners()
-    }
-
     function setTaskAsComplete(e) {
         let checkboxNumber = e.target.dataset.index
         let activeCategory = Category.categoryCollection.find(element => element.active === true);
@@ -340,26 +256,3 @@ const Tasks = (() => {
 })()
 
 export {Category, Tasks}
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-    
-
-
-    
-    
-
-
-
-
