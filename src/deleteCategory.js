1e321e67a094c1fa500edda1a22fa9173053d1c5
logeybogey <michaeldcjohnson@gmail.com>
2020-11-13 20:01:10 +0600

    deleteCategory linked up to localStorage

--- src/deleteCategory.js
+++ src/deleteCategory.js
@@ -3,12 +3,14 @@ import {Category} from './index.js'
 import {renderTasks} from './renderTasks.js'
 import {getCategoryCollection} from './localStorage.js'
 
+let categoryCollection = getCategoryCollection()
+
 const deleteCategory = (e) => {
     if (confirm("Delete Category?")) {   
         document.querySelectorAll('.tasksDisplay .completedTasksDisplay').forEach(task => task.remove());
         const index = e.target.dataset.index;
-        getCategoryCollection.splice(index, 1)
-        //localStorage.setItem('categoryCollection', JSON.stringify(categoryCollection));
+        categoryCollection.splice(index, 1)
+        localStorage.setItem('categoryCollection', JSON.stringify(categoryCollection));
         renderCategories()
         Category.setCategoryListeners()
         renderTasks()
